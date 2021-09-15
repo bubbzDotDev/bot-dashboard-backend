bdd8e909f6ce6be37189167260276d806ba86ac0
Samuel Krohn <SANIKR@outlook.com>
2021-09-15 00:06:32 -0600

    Initiate Typescript, add NPM packages, Create Hello World

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import path from "path"
+import indexRouter from "./routes/index"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/", indexRouter)
+
+export default app
